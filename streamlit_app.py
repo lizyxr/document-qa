1fb7c42b7cb13c0189b3505f051c5956a984c5ed
Liz Yu <ee07b282@gmail.com>
2024-09-13 16:51:07 -0700

    Try to run some demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,36 +2,26 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
+st.title("📄 Hackcup Demo")
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+problem = st.text_input("Problem", type="text")
+if not problem:
+    st.info("Please add your problem to continue.", icon="❓")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    #client = OpenAI(api_key=openai_api_key)
 
     # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+    uploaded_sample_input = st.file_uploader(
+        "Upload Sample Input (.txt)", type=("txt")
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
+    uploaded_sample_output = st.file_uploader(
+        "Upload Sample Output (.txt)", type=("txt")
     )
 
-    if uploaded_file and question:
+    if uploaded_sample_input and uploaded_sample_output:
 
         # Process the uploaded file and question.
         document = uploaded_file.read().decode()
@@ -43,11 +33,11 @@ else:
         ]
 
         # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
+        #stream = client.chat.completions.create(
+        #    model="gpt-3.5-turbo",
+        #    messages=messages,
+        #    stream=True,
+        #)
 
         # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        st.write_stream("sample output")
