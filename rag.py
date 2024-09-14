5cee7612c1e13a5b5cd72a18443c524ef69a1321
Liz Yu <ee07b282@gmail.com>
2024-09-13 17:20:22 -0700

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,1 +1,16 @@
+from openai import OpenAI
 
+class Rag:
+  problem = ""
+  input_data = ""
+  output_data = ""
+  
+  def __init__(self, problem, input_data, output_data):
+    self.problem = name
+    self.input_data = input_data
+    self.output_data = output_data
+
+  def run():
+    return "sample output"
+
+  
